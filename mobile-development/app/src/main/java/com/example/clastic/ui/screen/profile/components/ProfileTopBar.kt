446f14e39d2242e38200c0bd95f223128b9dbe61
Warren Gerald Polandra <85095564+warrenpolandra@users.noreply.github.com>
2023-06-07 10:56:41 +0700

    Fix profile screen

--- mobile-development/app/src/main/java/com/example/clastic/ui/screen/profile/components/ProfileTopBar.kt
+++ mobile-development/app/src/main/java/com/example/clastic/ui/screen/profile/components/ProfileTopBar.kt
@@ -1,16 +1,15 @@
 package com.example.clastic.ui.screen.profile.components
 
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.clastic.R
 import com.example.clastic.ui.theme.ClasticTheme
 
@@ -20,10 +19,12 @@ fun ProfileTopBar() {
         title = {
             Text(
                 text = stringResource(R.string.profile),
-                color = Color.White
+                color = Color.White,
+                modifier = Modifier
+                    .padding(start = 15.dp),
             )
         },
-        backgroundColor = colorResource(R.color.cyan_primary)
+        backgroundColor = colorResource(R.color.cyan_primary),
     )
 }
 
